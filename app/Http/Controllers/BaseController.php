862bada9ac0d09492ddcb429a70ccd470dbf6707
PaulChege <pchegenjenga@gmail.com>
2017-01-31 15:39:09 +0300

    auth

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paul
+ * Date: 31/01/2017
+ * Time: 15:28
+ */
+
+namespace App\Http\Controllers;
+
+use Dingo\Api\Routing\Helpers;
+use Illuminate\Routing\Controller;
+
+class BaseController extends Controller
+{
+    use Helpers;
+}
